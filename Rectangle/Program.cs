f328f7455a1b4187d790db8abf88293a60722919
Carolyn Hughett <cmhughett@gmail.com>
2019-08-27 11:43:04 -0500

    code practice assignment

--- Rectangle/Program.cs
+++ Rectangle/Program.cs
@@ -9,12 +9,18 @@ namespace Rectangle
             Console.Write("Enter the length of the rectangle: ");
 
             // Get input from the user and save it to a variable.
+            string lengthInput = Console.ReadLine();
+            int length = Convert.ToInt32(lengthInput);
 
             Console.Write("Enter the width of the rectangle: ");
 
             // Get input from the user and save it to a second variable.
+            string widthInput = Console.ReadLine();
+            int width = Convert.ToInt32(widthInput);
 
             // Output - The area of the rectangle is (computed value)
+            int area = length * width;
+            Console.WriteLine($"The area of the rectangle is " + area);
 
             Console.ReadLine();
         }
